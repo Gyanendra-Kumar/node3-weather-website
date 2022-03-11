58c435a88bd79aa6bf301c816c3a57d737855415
Huski-commando <gyan.mu@gmail.com>
2022-03-12 00:29:29 +0530

    Remove unnecessary console.log calll

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client side javascript is loading...')
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
